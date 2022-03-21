abc27fafa6916852962cf461dc24955bf8c5425b
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2022-03-21 21:12:14 +0200

    Initial Commit
    Created Solution from VS

---
+++ Truck Company Manager/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+/*
+Classes to be developed:
+1- Event Classes (Event Abstract)
+        - Preparation
+        - Cancel
+        - Promote
+2- Cargo and Truck Classes
+
+3- Company Class
+
+4- UI Class
+*/
+
+
+int main()
+{
+    
+}
+
