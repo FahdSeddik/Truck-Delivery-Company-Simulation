bc802c0f7908c487c09e36f62c428245967a6536
AbdulrahmanEl-Bedewy <bedoabdulrahman2002@hotmail.com>
2022-03-26 21:33:21 +0200

    UI class basic form

---
+++ Truck Company Manager/UI_Class.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class UI_Class
+{
+private:
+	int mode; // Decides the mode of the program interface
+public:
+	UI_Class();
+	void Print();
+};
+
